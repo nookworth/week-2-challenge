d7e96184fe74cb105c220823723f4fdf1748b227
nookworth <nooksack@gmail.com>
2022-09-11 09:15:44 -0600

    Added images to buttons.

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,10 +1,26 @@
 const passwordButton = document.getElementById("password-generator");
 const weatherButton = document.getElementById("weather-dashboard");
+const movieButton = document.getElementById("quickflix");
+const workButton = document.getElementById("workday-scheduler");
 
-passwordButton.addEventListener("click", function(){
-    window.open('https://nookworth.github.io/password-generator/', '_blank').focus();
+passwordButton.addEventListener("click", function () {
+  window
+    .open("https://nookworth.github.io/password-generator/", "_blank")
+    .focus();
 });
 
-weatherButton.addEventListener("click", function(){
-    window.open('https://nookworth.github.io/weather-dashboard/', '_blank').focus();
+weatherButton.addEventListener("click", function () {
+  window
+    .open("https://nookworth.github.io/weather-dashboard/", "_blank")
+    .focus();
+});
+
+movieButton.addEventListener("click", function () {
+  window.open("https://mich-hales.github.io/QuickFlix/", "_blank").focus();
+});
+
+workButton.addEventListener("click", function () {
+  window
+    .open("https://nookworth.github.io/work-day-scheduler/", "_blank")
+    .focus();
 });
